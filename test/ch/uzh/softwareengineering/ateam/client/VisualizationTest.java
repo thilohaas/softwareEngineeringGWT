d09ea4f5857acc7c18b4a6133d60070979333a6b
Andreas <andreas.perschak@bluewin.ch>
2013-11-17 15:45:15 +0100

    JUnit Test for Visualization

--- test/ch/uzh/softwareengineering/ateam/client/VisualizationTest.java
+++ test/ch/uzh/softwareengineering/ateam/client/VisualizationTest.java
@@ -1,15 +1,31 @@
 package ch.uzh.softwareengineering.ateam.client;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import com.google.gwt.user.client.ui.FlexTable;
 
 public class VisualizationTest {
 
 	@Test
 	public void test() {
+		Voting voting1 = new Voting();
+		Voting voting2 = new Voting();
+		Voting voting3 = new Voting();
 		
+		voting1.setTitle("Abzockerinitiative");
 		
+		Data data = new Data();
+		data.addVoting(voting1);
+		data.addVoting(voting2);
+		data.addVoting(voting3);
+		
+		Visualisation visual = new Visualisation();
+		visual.setData(data);
+		
+		FlexTable table = visual.visualize();
+		
+		assertEquals(4, table.getRowCount());
+		assertEquals(4, table.getCellCount(0));
 		
 	}
 

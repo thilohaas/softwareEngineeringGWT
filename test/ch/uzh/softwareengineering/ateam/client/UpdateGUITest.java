6391f2ae2a2f4b7fdc12cd7b4c7d412eb410ba65
Tobias Bächle <tb@tandt-production.com>
2013-12-01 22:03:23 +0100

    added JUnit test for updateGUI()

---
+++ test/ch/uzh/softwareengineering/ateam/client/UpdateGUITest.java
@@ -0,0 +1,16 @@
+package ch.uzh.softwareengineering.ateam.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UpdateGUITest {
+
+	@Test
+	public void test() {
+		VotingGUI appGUI = new VotingGUI();
+		appGUI.buildGUI();
+		appGUI.updateGUI();	
+	}
+
+}

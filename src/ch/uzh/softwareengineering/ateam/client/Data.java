498a634c67a6a7354fd2919395a92f7727c63215
Andreas <andreas.perschak@bluewin.ch>
2013-12-01 20:19:59 +0100

    Tabellaric View is finished. All votings are sorted by year and than by title. Everytime that someone adds a vote, it is inserted at the place corresponding to its order. I also implemented the quicksort algorithm which can be used at a later point in the development process.

--- src/ch/uzh/softwareengineering/ateam/client/Data.java
+++ src/ch/uzh/softwareengineering/ateam/client/Data.java
@@ -25,6 +25,25 @@ public class Data implements VotingService{
 
 	@Override
 	public Voting addVoting(Voting voting) {
+		if (size == 0){  //immediately insert the vote if the set is empty. 
+			dataset.add(voting);
+			size++;
+			return null;
+		}
+		for(int i = 0; i < size; i++){
+			if (voting.getYear() > dataset.get(i).getYear()){  //insert the vote at position i, if it is 'younger' than the vote at this time 
+				dataset.add(i, voting); 
+				size++;
+				return null;
+			}
+			if (voting.getYear() == dataset.get(i).getYear()){ // If two votes have the same age, check if the new one lexicographically precedes the old one. 
+				if(voting.getTitle().compareTo(dataset.get(i).getTitle()) < 0){
+					dataset.add(i, voting);
+					size++;
+					return null;
+				}
+			}
+		}
 		dataset.add(voting);
 		size++;
 		return null;
@@ -34,4 +53,44 @@ public class Data implements VotingService{
 		return size;
 	}
 	
+	public void quickSort(int left, int right){
+		if (left >= right){
+			return;
+		}
+		
+		int j = partition(left, right);
+		
+		quickSort(left, j -1);
+		quickSort(j + 1, right); 
+	}
+	
+	public int partition(int left, int right){
+		int j = left;
+		int k = right -1;
+		int pivot = dataset.get(right).getYear();
+	
+		while (j < k){
+			while (dataset.get(j).getYear() <= pivot && j < right){
+				j++;
+			}
+		
+			while (dataset.get(k).getYear() >= pivot && k > left){
+				k--;
+			}
+		
+			if (j < k){
+				Voting temp = dataset.get(j);
+				dataset.set(j, dataset.get(k));
+				dataset.set(k, temp);
+			}
+		}
+	
+		if (dataset.get(j).getYear() > pivot){
+			Voting temp = dataset.get(j);
+			dataset.set(j, dataset.get(right));
+			dataset.set(right, temp);
+		}
+		
+		return j; 
+	}
 }
